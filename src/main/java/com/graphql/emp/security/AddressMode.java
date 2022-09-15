40393d499e6eac165a0958e5be219684252fd3cb
shanuchauhan94 <shanuchauhan94@gmail.com>
2022-09-15 11:00:16 +0530

    security folder removed

--- src/main/java/com/graphql/emp/security/AddressMode.java
+++
@@ -1,9 +0,0 @@
-package com.graphql.emp.security;
-
-public enum AddressMode {
-
-    UNKNOWN,
-    MEM_32BIT, // 32-bit address mode using 32-bit reference
-    MEM_64BIT, // 64-bit address mode using 32-bit reference
-    MEM_64BIT_COMPRESSED_OOPS
-}
